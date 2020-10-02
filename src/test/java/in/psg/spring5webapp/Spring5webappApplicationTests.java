826edd0bc2dcb91f69e2d4e39b5b3aa887f19f28
Pekal Saigopal <psgopal88@gmail.com>
2020-10-02 20:07:00 +0530

    Initial Commit Author and Book POJO Class -ManyToMany relationship between Author and Book -equal and hashcode method using id attribute -toString method

---
+++ src/test/java/in/psg/spring5webapp/Spring5webappApplicationTests.java
@@ -0,0 +1,13 @@
+package in.psg.spring5webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5webappApplicationTests {
+
+   @Test
+   void contextLoads() {
+   }
+
+}
